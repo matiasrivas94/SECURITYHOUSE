8c01e0c3677a7f305f69fbbcf7dc64bba2779a4e
maury07 <maury.-n@live.com.ar>
2018-10-24 22:55:49 -0300

    Modifique fragment de configuracion con una vista con otros iconos

--- app/src/main/java/com/example/matiasezequiel/security_house/ConfigFragment.java
+++ app/src/main/java/com/example/matiasezequiel/security_house/ConfigFragment.java
@@ -3,6 +3,7 @@ package com.example.matiasezequiel.security_house;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -29,6 +31,8 @@ public class ConfigFragment extends Fragment {
     String desactivar = clave + "DS";
     String nuevaclave="0987";
 
+    ImageView iv_desactivar, iv_activar, iv_cambiar, iv_estado;
+
     int tipoConfig;
 
     @Override
@@ -37,21 +41,10 @@ public class ConfigFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_config, container, false);
 
-        opciones = (Spinner) v.findViewById(R.id.spPrueba);
-
-        //Shared para mostrar el tipo de la alarma en el spinner
-        SharedPreferences prefs = getContext().getSharedPreferences("tipoAlarmaSpinner",Context.MODE_PRIVATE);
-        tipoConfig = (int)prefs.getLong("configSpinner",-1);
-
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.alarmas, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.preference_category);
-        opciones.setAdapter(adapter);
-
 
             //BOTON ACTIVAR
-            Button btnActivar = (Button) v.findViewById(R.id.btnActivar);
-            btnActivar.setOnClickListener(new View.OnClickListener() {
+            iv_activar = (ImageView) v.findViewById(R.id.btnActivar);
+            iv_activar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mm.enviarMensaje(numero, activar);
@@ -59,8 +52,8 @@ public class ConfigFragment extends Fragment {
             });
 
             //BOTON DESACTIVAR
-            Button btnDesactivar = (Button) v.findViewById(R.id.btnDesactivar);
-            btnDesactivar.setOnClickListener(new View.OnClickListener() {
+            iv_desactivar = (ImageView) v.findViewById(R.id.btnDesactivar);
+            iv_desactivar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mm.enviarMensaje(numero, desactivar);
@@ -68,8 +61,8 @@ public class ConfigFragment extends Fragment {
             });
 
             //BOTON CAMBIAR CLAVE
-            Button btnCambiar = (Button) v.findViewById(R.id.btnCambiarClave);
-            btnCambiar.setOnClickListener(new View.OnClickListener() {
+            iv_cambiar = (ImageView) v.findViewById(R.id.btnCambiarClave);
+            iv_cambiar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mm.enviarMensaje("Clave Vieja" + clave + "CU", "Clave Nueva" + nuevaclave);
