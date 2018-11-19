710d92e380cc58bb11b3b13e4c008344b8c6716b
maury07 <maury.-n@live.com.ar>
2018-11-19 13:37:46 -0300

    Arreglamos la clase MiMensaje para que no se rompa cuando no se encuentra el numero del mensaje que llegó Arreglamos fragmentContacto para que la letra de los EditText no se vean blancos

--- app/src/main/java/com/example/matiasezequiel/security_house/MiMensaje.java
+++ app/src/main/java/com/example/matiasezequiel/security_house/MiMensaje.java
@@ -61,56 +61,47 @@ public class MiMensaje extends BroadcastReceiver {
                 celular = mensajes[i].getOriginatingAddress();// https://developer.android.com/reference/android/telephony/SmsMessage#getOriginatingAddress()
                 textomensaje = mensajes[i].getMessageBody();// Devuelve el cuerpo del mensaje como una Cadena, si existe y está basada en texto.
 
-                //Cel tien los ultimos 10 numeros del numero del mensaje recibido de la alarma
-                String cel = celular.substring(celular.length()-10);
+                if(celular.length() >= 10){
 
-                //Funcion para recuperar el ultimo caracter(entero) del mensaje para saber que ZONA hay actividad
-                if((mensajes[i].getOriginatingAddress()).equals(celular)) {
-                    ultimo = textomensaje.charAt(textomensaje.length() - 1);
-                    int x = ultimo - '0';
-                    if ((x >= 1) && (x <= 6)) {
-                        try {
-                            a = ((BaseAplication) context.getApplicationContext()).getAlarmaNum(cel);
-                            if((a.size() == 1)) {
-                                //metodo que devuelve una alarma segun el numero de telefono
-                                nombreAlarm = a.get(0).getNombre();
+                    //Cel tien los ultimos 10 numeros del numero del mensaje recibido de la alarma
+                    String cel = celular.substring(celular.length()-10);
+                    try{
+                        a = ((BaseAplication) context.getApplicationContext()).getAlarmaNum(cel);
+                        //Funcion para recuperar el ultimo caracter(entero) del mensaje para saber que ZONA hay actividad
+                        ultimo = textomensaje.charAt(textomensaje.length() - 1);
+                        int x = ultimo - '0'; //ASCII
+                        if ((x >= 1) && (x <= 6)) {
+                            //metodo que devuelve una alarma segun el numero de telefono
+                            nombreAlarm = a.get(0).getNombre();
 
-                                //selecciono todas las zonas almacenadas segun el id de la alarma que traigo de arriba
-                                //
-                                ArrayList<Zona> zonas = ((BaseAplication) context.getApplicationContext()).getZonas(a.get(0).getIdAlarma());
-                                final ArrayList<Integer> idZonas = new ArrayList<>();
-                                for (int y = 0; y < zonas.size(); y++) {
-                                    idZonas.add(zonas.get(y).getIdZona());
-                                }
-                                int y = idZonas.get(x - 1);
-                                for (int z = 0; z < idZonas.size(); z++) {
-                                    if (idZonas.get(z) == y) {
-                                        long res = ((BaseAplication) context.getApplicationContext()).updateNotiZona(idZonas.get(z), 1);
-                                        Calendar c = Calendar.getInstance();
-                                        hora = c.getTime().toString();
-                                        SharedPreferences.Editor editor = context.getSharedPreferences("time", Context.MODE_PRIVATE).edit();
-                                        editor.putString("hora", hora);
-                                        editor.commit();
-                                        /*if (res > 0)
-                                            Toast.makeText(context, "Noti Actualizada", Toast.LENGTH_LONG).show();
-                                        else
-                                            Toast.makeText(context, "No se pudo actualizar la Notificacion", Toast.LENGTH_LONG).show();*/
-                                    }
-                                }
-                                SharedPreferences.Editor editor = context.getSharedPreferences("cc", Context.MODE_PRIVATE).edit();
-                                editor.putLong("idAlarma", a.get(0).getIdAlarma());
-                                editor.commit();
-
-
-                                Notificar(context);
+                            //selecciono todas las zonas almacenadas segun el id de la alarma que traigo de arriba
+                            ArrayList<Zona> zonas = ((BaseAplication) context.getApplicationContext()).getZonas(a.get(0).getIdAlarma());
+                            final ArrayList<Integer> idZonas = new ArrayList<>();
+                            for (int y = 0; y < zonas.size(); y++) {
+                                idZonas.add(zonas.get(y).getIdZona());
                             }
-                        }catch (Exception e) {
-                            Log.e("Error", e.getMessage());
-                            e.printStackTrace();
+                            int y = idZonas.get(x - 1);
+                            for (int z = 0; z < idZonas.size(); z++) {
+                                if (idZonas.get(z) == y) {
+                                    long res = ((BaseAplication) context.getApplicationContext()).updateNotiZona(idZonas.get(z), 1);
+                                    Calendar c = Calendar.getInstance();
+                                    hora = c.getTime().toString();
+                                    SharedPreferences.Editor editor = context.getSharedPreferences("time", Context.MODE_PRIVATE).edit();
+                                    editor.putString("hora", hora);
+                                    editor.commit();
+                                }
+                            }
+                            SharedPreferences.Editor editor = context.getSharedPreferences("cc", Context.MODE_PRIVATE).edit();
+                            editor.putLong("idAlarma", a.get(0).getIdAlarma());
+                            editor.commit();
+                            Notificar(context);
                         }
                     }
+                    catch (Exception e){
+                        Log.e("Error", e.getMessage());
+                        e.printStackTrace();
+                    }
                 }
-
             }
         }
     }
